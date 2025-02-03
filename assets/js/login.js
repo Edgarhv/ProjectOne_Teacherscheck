7467e285452eaa8c5eccdde2ebc8aa2b76dee7ad
MichGuzman <michguzman@live.com>
2025-02-03 17:21:47 -0600

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -10,7 +10,7 @@ document.getElementById('loginButton').addEventListener('click', function() {
     // Validar que el nombre esté en la lista de maestros
     if (teachers.includes(teacherName)) {
         // Si el nombre es válido, redirigir a la página de estudiantes
-        window.location.href = 'student_form.html'; // Asegúrate de que esta ruta sea correcta
+        window.location.href = '../pages/studentForms.html'; // Asegúrate de que esta ruta sea correcta
     } else {
         // Si el nombre no es válido, mostrar un mensaje de error
         console.log('No valid Name'); // Muestra en consola que el nombre no es válido
