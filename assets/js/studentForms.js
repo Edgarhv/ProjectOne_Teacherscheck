6a1f8daf194fe13197ae623c5c587b01cf9e5e89
Admin <admin@Major-Tom-2.local>
2025-02-03 23:22:28 -0600

    JS validation and input reset done

--- assets/js/studentForms.js
+++ assets/js/studentForms.js
@@ -8,6 +8,10 @@ let saveButton = document.getElementById('saveStudent');
 const studentsArray = JSON.parse(localStorage.getItem('studentsArray')) || [];
 //select student info Button from html
 let studentInfoBtn = document.getElementById('studentInfoBtn');
+// Constants needed for validation
+const isString = /^[a-zA-Z ]+$/;
+const isDigit = /^\d{10}$/;
+const emailSyntax = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/g;
 
 function saveStudentForm (event) {
     console.log('hi from the function saveStudentForm');
@@ -33,7 +37,53 @@ function goToStudentInfo (event) {
 function moveUrl (url) {
     document.location.href = (url);
 }
-saveButton.addEventListener('click', saveStudentForm);
+
+function notvalid() {
+    if (!isString.test(studentName.value) || !emailSyntax.test(email.value) || (!isDigit.test(contact.value) && contact.value.length !== 10) || !birthdate.value) {
+        return true;
+    };
+    return false;
+};
+
+function validationAlert() {
+    const errorObject = {};
+
+    if (!isString.test(studentName.value)) {
+        errorObject.nameError = "\n - Enter a valid name"
+    };
+    if (!birthdate.value) {
+        errorObject.dateError = "\n - Enter a valid date"
+    };
+    if (!emailSyntax.test(email.value)) {
+        errorObject.emailError = "\n - Enter a valid e-mail"
+    };
+    if (!isDigit.test(contact.value)) {
+        errorObject.contactError = "\n - Enter a valid number"
+    };
+
+    if (errorObject) {
+        let errorText = "The following error(s) raised!"
+        for (const key in errorObject) {
+         errorText += errorObject[key]
+        }
+        alert(errorText)
+    }
+};
+
+saveButton.addEventListener('click', function(){
+    // Validation filter
+    if (notvalid()) {
+        validationAlert()
+    } else {
+    saveStudentForm();
+    // Reset input value
+    studentName.value = "";
+    birthdate.value = "";
+    email.value = "";
+    contact.value = "";
+    }
+});
+
 studentInfoBtn.addEventListener('click', goToStudentInfo);
 
 
