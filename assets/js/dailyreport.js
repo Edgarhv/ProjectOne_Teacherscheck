faa4e984a987fe3be4b7d91a713b95f9889f1c54
Edgar Hernandez <85400161+Edgarhv@users.noreply.github.com>
2025-02-04 21:16:51 -0600

    new version

--- assets/js/dailyreport.js
+++ assets/js/dailyreport.js
@@ -1,18 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
     const tableBody = document.getElementById("studentTableBody");
+    const reportButton = document.getElementById("generateReport");
 
-    // Obtain data from local storage
-    let students = JSON.parse(localStorage.getItem("students")) || [];
+    // Function 
+    function loadReportData() {
+        let report = JSON.parse(localStorage.getItem("reportObject")) || { assits: 0, absence: 0 };
 
-    // Count present and absent student
-    let presentCount = students.filter(student => student.present === "yes").length;
-    let absentCount = students.filter(student => student.present === "no").length;
+        console.log("Datos del reporte:", report); 
 
-    // Generate count students
-    tableBody.innerHTML = `
-        <tr>
-            <td><strong>${presentCount}</strong></td>
-            <td><strong>${absentCount}</strong></td>
-        </tr>
-    `;
+        let totalStudents = report.assits + report.absence;
+
+        // Insert data in the local storage
+        tableBody.innerHTML = `
+            <tr>
+                <td><strong>${report.assits}</strong></td>
+                <td><strong>${report.absence}</strong></td>
+                <td><strong>${totalStudents}</strong></td>
+            </tr>
+        `;
+    }
+
+    // Action of the botton "REPORT"
+    reportButton.addEventListener("click", loadReportData);
 });
