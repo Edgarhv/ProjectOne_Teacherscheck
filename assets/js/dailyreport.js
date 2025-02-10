3b2a2770707835df38b0c624bfe740166f2efcd3
Edgar Hernandez <85400161+Edgarhv@users.noreply.github.com>
2025-02-10 17:19:35 -0600

    Add files via upload

--- assets/js/dailyreport.js
+++ assets/js/dailyreport.js
@@ -1,25 +1,67 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const tableBody = document.getElementById("studentTableBody");
-    const reportButton = document.getElementById("generateReport");
+    
+    const updateReportButton = document.getElementById("updateReport");
 
-    // Function 
-    function loadReportData() {
-        let report = JSON.parse(localStorage.getItem("reportObject")) || { assits: 0, absence: 0 };
-
-        console.log("Datos del reporte:", report); 
-
-        let totalStudents = report.assits + report.absence;
+    
+    function updateReportData() {
+        
+        let report = JSON.parse(localStorage.getItem("reportObject")) || { assists: 0, absence: 0, male: 0, female: 0 };
+        
+        
+        const reportTableBody = document.getElementById("reportTableBody");
+        const totalStudents = report.assists + report.absence;
 
         // Insert data in the local storage
-        tableBody.innerHTML = `
+        reportTableBody.innerHTML = `
             <tr>
-                <td><strong>${report.assits}</strong></td>
+                <td><strong>${report.assists}</strong></td>
                 <td><strong>${report.absence}</strong></td>
                 <td><strong>${totalStudents}</strong></td>
             </tr>
         `;
+
+        
+        const ctx = document.getElementById('genderChart').getContext('2d');
+        
+        
+        if (window.genderChart && typeof window.genderChart.destroy === 'function') {
+            window.genderChart.destroy(); 
+        }
+
+        // New Graphic
+        window.genderChart = new Chart(ctx, {
+            type: 'bar', 
+            data: {
+                labels: ['MALE', 'FEMALE'], 
+                datasets: [{
+                    label: 'NUMBER OF STUDENTS',
+                    data: [report.male, report.female], 
+                    backgroundColor: ['#010bff', '#ff4d9b'], 
+                    borderColor: ['#0056b3', '#e94d30'], 
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
     }
 
-    // Action of the botton "REPORT"
-    reportButton.addEventListener("click", loadReportData);
+    // Action of the botton
+    updateReportButton.addEventListener("click", function () {
+        
+        let report = JSON.parse(localStorage.getItem("reportObject")) || { assists: 0, absence: 0, male: 0, female: 0 };
+
+        
+        updateReportData();
+    });
+
+    
+    updateReportData();
 });
+
